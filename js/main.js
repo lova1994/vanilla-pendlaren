e8b04e334953275440eb4890b7a3ccb2f45e7e65
Lova Wins <lovaunger@gmail.com>
2020-05-06 22:02:29 +0200

    borjat med funktion for hallplats

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ function getLocation() {
         (position) => {
             let currentLocation = position.coords;
             nearStops(currentLocation);       
-
         }
     )
   } else { 
@@ -16,21 +15,12 @@ function getLocation() {
 }
 
 
-
-
 async function nearStops(currentLocation) {
     const apiKey = '32808697-8cef-4cdc-81a3-477f899fd378'
     const baseUrl = 'https://api.resrobot.se/v2/location.nearbystops?';
-    // let latitude = 59.5197344;
-    // let longitude = 17.8927437;
 
     let latitude = currentLocation.latitude;
-    console.log(latitude)
-
     let longitude = currentLocation.longitude;
-    console.log(longitude)
-
-    
     let URL = `${baseUrl}key=${apiKey}&originCoordLat=${latitude}&originCoordLong=${longitude}&format=json`
 
     try {
@@ -40,20 +30,23 @@ async function nearStops(currentLocation) {
         console.log(stops);
         console.log(stops.StopLocation[0]);
 
+        let id = stops.StopLocation[0].id;
+        console.log("id " + id)
+
+
+        departures(id) // vald hållplats ska skickas in här till departures
+
 
         for (i=0; i < 10; i++) {
             console.log(stops.StopLocation[i].name) 
         }
            
-
     } catch (err) {
         console.log(err)
     }
 }
 
-async function departures() {
-
-
+async function departures(id) {
     console.log("departures ran")
     const apiKey = '92eb7245-c121-4899-90dc-059f68233948'
     const baseUrl = 'https://api.resrobot.se/v2/departureBoard?';
@@ -66,7 +59,7 @@ async function departures() {
     // let longitude = currentLocation.longitude;
     // console.log(longitude)
 
-    let id = "740066787";
+    
     
     let URL = `${baseUrl}key=${apiKey}&id=${id}&format=json`
 
@@ -80,7 +73,7 @@ async function departures() {
     }
 }
 
-departures()
+
 
 
 

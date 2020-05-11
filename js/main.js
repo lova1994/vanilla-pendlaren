11d8261cc8e389a8caf0e91b2a48505bc897ff02
Lova Wins <lovaunger@gmail.com>
2020-05-11 16:02:03 +0200

    departures function works now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var x = document.getElementById("show-position");
+// const testBtn = document.getElementById("testBtn");
+// testBtn.addEventListener('click', departures);
+
+// function testMe() {
+//     console.log("Event listener works")
+// }
+
+
 
 function getLocation() {
     console.log("Hämtar din position...")
@@ -15,12 +22,6 @@ function getLocation() {
 }
 
 
-function getId() {
-    let id = 740000782;
-    console.log("Get id ran")
-    departures(id)
-}
-
 async function nearStops(currentLocation) {
 
     console.log("Hittar närmaste hållplatser...")
@@ -32,44 +33,61 @@ async function nearStops(currentLocation) {
     let URL = `${baseUrl}key=${apiKey}&originCoordLat=${latitude}&originCoordLong=${longitude}&format=json`
     // let id = 740000782;
 
- 
-
     try {
         let resp = await fetch(URL);
         let stops = await resp.json();
         let i;
         console.log("Hämtar hållplatser...");
-    
-        // departures(id) // vald hållplats ska skickas in här till departures
-
      
         for (i=0; i < 10; i++) {
             let id = stops.StopLocation[i].id;
             let stop = stops.StopLocation[i].name;
-            // console.log("Hållplats-id:" + id) // MAN SKA KUNNA KLICKA PÅ EN HÅLLPLATS som skickar med id
-            // console.log("Hållplats-namn:" + stop)
-            document.getElementById("stops").innerHTML += `<p data-stop-id="${id}">  ${stop} </p> 
+            document.getElementById("stops").innerHTML += `<p id="myStop" class="stops">  ${stop} </p>
+            <button id="pickStation" stop-id="${id}" > Visa avgångar  </button>`;
 
-            <button onclick="getId()" > Add  </button>            
-           `;
-            // document.getElementById("stops").innerHTML += `<p> ${stops.StopLocation[i].id} </p>`;
+        }
+
         
-        
-        // loopa ut en knapp som har varje hållplats id, 
-        // när du klickar den körs funktionen som skickar id hållplats till departures()
-    
-     
-    
-    }
 
+    // const pickStation = document.getElementById("pickStation");
 
+    // pickStation.addEventListener('click', (event) => {
+    //     const stationID = document.getElementById("myStop").getAttribute('stop-id');
+    //     console.log("Hämtar stations ID: " + stationID)
+    //     departures(stationID)
+    // });
 
-
-           
+    addEventsListeners();
     } catch (err) {
         console.log(err)
     }
 }
+
+function addEventsListeners() {
+    //Hämtar alla stationer med klassen .stops
+    //Sätter en event listener på varje station 
+    const pickStation = document.querySelectorAll("#pickStation");
+    for(let i = 0; i < pickStation.length; i++) {
+        pickStation[i].addEventListener('click', (event) => {
+            //Hämtar stop-id från det elementet man klickade på
+            const stationID = event.srcElement.getAttribute('stop-id');
+            console.log("Hämtar stations ID: " + stationID)
+            departures(stationID)
+        });
+    }
+}
+
+// function addEventsListeners() {
+//     //Hämtar alla stationer med klassen .stops
+//     //Sätter en event listener på varje station 
+//     const pickStation = document.querySelector("#stops");
+//     pickStation.addEventListener('click', (event) => {
+//         //Hämtar stop-id från det elementet man klickade på
+//         const stationID = event.srcElement.getAttribute('stop-id');
+//         console.log("Hämtar stations ID: " + stationID)
+//         departures(stationID)
+//     });
+// }
 
 // function sendId(){
     
@@ -77,28 +95,23 @@ async function nearStops(currentLocation) {
 //     departures(id)
 // }
 
-
-async function departures() {
-    console.log("Hämtar avgång")
+async function departures(id) {
+    console.log("Hämtar avgång...")
     const apiKey = '92eb7245-c121-4899-90dc-059f68233948'
     const baseUrl = 'https://api.resrobot.se/v2/departureBoard?';
     // https://api.resrobot.se/v2/departureBoard?key=92eb7245-c121-4899-90dc-059f68233948&id=740000782&format=json
     
 
-    let id = 740000782; // Hållplats ID
+    // let id = 740000782; // Hållplats ID
     let URL = `${baseUrl}key=${apiKey}&id=${id}&format=json`;
     let el =  document.getElementById('departures');
     try {
         let resp = await fetch(URL);
         // console.log("svar " + resp)
-        let departures = await resp.json();
-        // console.log( "DEEPARTUTRES: " + departures)
-
+        let departures = await resp.json();        
 
         for(departure of departures.Departure) {
-            console.log('Loopar alla avgångar...')
-            // console.log(departure);
-            // console.log(departure.direction);
+            console.log('Loopar ut alla avgångar...')
 
             el.innerHTML += `
             <p> ${departure.direction}</p>
@@ -110,11 +123,9 @@ async function departures() {
             // console.log(departure.name);
             // console.log(departure.date);
             // console.log(departure.time);
+            
     }
-
-        // return departures; 
-        
-
+    console.log(departure.stop)
 
     } catch (err) {
         console.log(err)
@@ -122,4 +133,4 @@ async function departures() {
 }
 
 getLocation()
-departures()
+// departures()
